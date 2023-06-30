658a1663f3b2c63bb000ec23bea15645b3c6348b
carocadieux <caroline-cadieux@outlook.com>
2023-06-30 10:34:31 -0400

    installer bootstrap creer composants

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+//import Stack from "react-bootstrap/Stack";
+import Button from "react-bootstrap/Button";
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Container fluid>
+
+      <Container>
+        <Row>
+          <Col>
+          </Col>
+
+          <Col>
+          </Col>
+        </Row>
+
+      </Container>
+
+     </Container>
+    </>
+  )
+}
+
+export default App
